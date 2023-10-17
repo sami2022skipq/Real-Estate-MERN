6395ab3d2aa3e2f91dc7d9fac640106597473be7
sami2022skipq <abdul.sami.skipq@gmail.com>
2023-10-17 20:44:35 +0500

    routes created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import  {BrowserRouter, Routes, Route} from 'react-router-dom' 
+import Home from './pages/Home'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIn'
+import SignOut from './pages/SignOut'
 
 export default function App() {
   return (
-    <h1 className='text-green-950 bg-blue-200'>This is the app</h1>
+  <BrowserRouter>
+  <Routes>
+    <Route path='/' element={<Home/>} />
+    <Route path='/about' element={<About/>} />
+    <Route path='/profile' element={<Profile/>} />
+    <Route path='/sign-in' element={<SignIn/>} />
+    <Route path='/sign-out' element={<SignOut/>} />
+  </Routes>
+  
+  </BrowserRouter>
   )
 }
 
