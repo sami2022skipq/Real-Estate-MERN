8b349f0575fcf956f86fb892934e7975160ecaeb
sami2022skipq <abdul.sami.skipq@gmail.com>
2023-10-18 19:45:34 +0500

    user model update

---
+++ api/models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema(
+    {
+        name:{
+            type:String,
+            require:true,
+            unique:true
+        },
+        email:{
+            type:String,
+            require:true,
+            unique:true
+        },
+        password:{
+            type:String,
+            require:true,
+            unique:true
+        }
+    },{timestamps:true}
+)
+const User = mongoose.model('User', userSchema)
+
+export default User
